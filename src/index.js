08ab7421dd10d374b239b2c8373a9ec170296235
itsmechrissyj <mrscjones1985@gmail.com>
2023-02-24 21:26:54 +0000

    Adding functioning world clocks JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
+setInterval(function () {
+  // madrid
+  let madridElement = document.querySelector("#madrid");
+  let madridDateElement = madridElement.querySelector(".date");
+  let madridTimeElement = madridElement.querySelector(".time");
+  let madridTime = moment().tz("Europe/Madrid");
 
+  madridDateElement.innerHTML = madridTime.format("MMMM Do YYYY");
+  madridTimeElement.innerHTML = madridTime.format(
+    "h:mm:ss [<small>]a[</small>]"
+  );
+
+  // lisbon
+  let lisbonElement = document.querySelector("#lisbon");
+  let lisbonDateElement = lisbonElement.querySelector(".date");
+  let lisbonTimeElement = lisbonElement.querySelector(".time");
+  let lisbonTime = moment().tz("Europe/Lisbon");
+
+  lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
+  lisbonTimeElement.innerHTML = lisbonTime.format(
+    "h:mm:ss [<small>]a[</small>]"
+  );
+
+  // london
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]a[</small>]"
+  );
+}, 1000);
