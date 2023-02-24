5658e7190d701e0f27a267d5c8f561689d676101
itsmechrissyj <mrscjones1985@gmail.com>
2023-02-24 22:11:30 +0000

    Adding JS for Select

--- src/index.js
+++ src/index.js
@@ -10,17 +10,6 @@ setInterval(function () {
     "h:mm:ss [<small>]a[</small>]"
   );
 
-  // lisbon
-  let lisbonElement = document.querySelector("#lisbon");
-  let lisbonDateElement = lisbonElement.querySelector(".date");
-  let lisbonTimeElement = lisbonElement.querySelector(".time");
-  let lisbonTime = moment().tz("Europe/Lisbon");
-
-  lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
-  lisbonTimeElement.innerHTML = lisbonTime.format(
-    "h:mm:ss [<small>]a[</small>]"
-  );
-
   // london
   let londonElement = document.querySelector("#london");
   let londonDateElement = londonElement.querySelector(".date");
@@ -31,4 +20,34 @@ setInterval(function () {
   londonTimeElement.innerHTML = londonTime.format(
     "h:mm:ss [<small>]a[</small>]"
   );
+
+  // costa rica
+  let costaRicaElement = document.querySelector("#costaRica");
+  let costaRicaDateElement = costaRicaElement.querySelector(".date");
+  let costaRicaTimeElement = costaRicaElement.querySelector(".time");
+  let costaRicaTime = moment().tz("America/Costa_Rica");
+
+  costaRicaDateElement.innerHTML = costaRicaTime.format("MMMM Do YYYY");
+  costaRicaTimeElement.innerHTML = costaRicaTime.format(
+    "h:mm:ss [<small>]a[</small>]"
+  );
 }, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+        <div>
+          <h2> ${cityName} </h2>
+          <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss [<small>]a[</small>]"
+        )}</div>
+      </div>`;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
